cc2640e94415de43b49a9e99bd84959cb61234ec
Nikhita Desai <nikhita2001desai@gmail.com>
2025-12-22 19:44:17 +0530

    updating female image changes

--- extensions/traya-checkout-dev/src/Checkout.jsx
+++ extensions/traya-checkout-dev/src/Checkout.jsx
@@ -8,30 +8,71 @@ import {
   usePhone,
   useSettings,
   useShippingAddress,
+  useCartLines,
+  useApplyCartLinesChange,
 } from "@shopify/ui-extensions-react/checkout";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default reactExtension("purchase.checkout.block.render", () => (
-  <Extension /> 
+  <Extension />
 ));
 
 function Extension() {
   const changeAttribute = useApplyAttributeChange();
+  const applyCartLinesChange = useApplyCartLinesChange();
+  const changeAddress = useApplyShippingAddressChange();
+
   const Attributes = useAttributes();
-  const lastAttribute = Attributes.filter((attribute) => {
-    if (attribute.key === "prepaid") {
-      return attribute; 
-    }
-  });
-  function formatPhoneNumber(phoneNumber) {
-    let formattedNumber = phoneNumber;
-    if (formattedNumber !== undefined && formattedNumber !== null) {
-      formattedNumber = phoneNumber?.replace(/^\+91/, "");
-      formattedNumber = formattedNumber?.replace(/^0/, "");
-    }
-    return formattedNumber;
-  }
-  var {
+  const cartLines = useCartLines();
+  const ShippingAddress = useShippingAddress();
+
+  const email = useEmail();
+  const phone = usePhone();
+
+  const addressUpdatedRef = useRef(false);
+
+  /* ---------------- FREE PRODUCT QTY FIX ---------------- */
+  const FREE_PRODUCT_VARIANT_ID =
+    "gid://shopify/ProductVariant/46566505087232";
+
+  useEffect(() => {
+    console.log('version 10')
+    cartLines.forEach((line) => {
+      if (
+        line.merchandise.id === FREE_PRODUCT_VARIANT_ID &&
+        line.quantity > 1
+      ) {
+        applyCartLinesChange({
+          type: "updateCartLine",
+          id: line.id,
+          quantity: 1,
+        });
+      }
+    });
+  }, [cartLines]);
+
+  /* ---------------- SETTINGS ---------------- */
+  let {
+    pincode1 = "201301",
+    pincode2 = "",
+    pincode3 = "",
+    pincode4 = "",
+    pincode5 = "",
+    pincode6 = "",
+    pincode7 = "",
+    phone_numbers = "9058222810",
+  } = useSettings();
+
+  pincode2 ||= pincode1;
+  pincode3 ||= pincode1;
+  pincode4 ||= pincode1;
+  pincode5 ||= pincode1;
+  pincode6 ||= pincode1;
+  pincode7 ||= pincode1;
+
+  const restrictPhones = phone_numbers.split(",");
+
+  const zipArrays = [
     pincode1,
     pincode2,
     pincode3,
@@ -39,293 +80,119 @@ function Extension() {
     pincode5,
     pincode6,
     pincode7,
-    phone_numbers,
-  } = useSettings();
-  if (pincode1 === null || pincode1 === undefined) {
-    pincode1 = "201301";
-  }
-  if (pincode2 === null || pincode2 === undefined) {
-    pincode2 = "";
-  }
-  if (pincode3 === null || pincode3 === undefined) {
-    pincode3 = "";
-  }
-  if (pincode4 === null || pincode4 === undefined) {
-    pincode4 = "";
-  }
-  if (pincode5 === null || pincode5 === undefined) {
-    pincode5 = "";
-  }
-  if (pincode6 === null || pincode6 === undefined) {
-    pincode6 = "";
-  }
-  if (pincode7 === null || pincode7 === undefined) {
-    pincode7 = "";
-  }
-  if (pincode2 === "") {
-    pincode2 = pincode1;
-  }
-  if (pincode3 === "") {
-    pincode3 = pincode1;
-  }
-  if (pincode4 === "") {
-    pincode4 = pincode1;
-  }
-  if (pincode5 === "") {
-    pincode5 = pincode1;
-  }
-  if (pincode6 === "") {
-    pincode6 = pincode1;
-  }
-  if (pincode7 === "") {
-    pincode7 = pincode1;
-  }
-  if (phone_numbers === null || phone_numbers === undefined) {
-    phone_numbers = "9058222810";
-  }
-  const restrict_phones = phone_numbers.split(",");
-  const zip1 = pincode1.split(","),
-    zip2 = pincode2.split(","),
-    zip3 = pincode3.split(","),
-    zip4 = pincode4.split(","),
-    zip5 = pincode5.split(","),
-    zip6 = pincode6.split(","),
-    zip7 = pincode7.split(",");
-  const zipArrays = [zip1, zip2, zip3, zip4, zip5, zip6, zip7];
-  const ShippingAddress = useShippingAddress();
-  const changeAddress = useApplyShippingAddressChange();
-  const email = useEmail(),
-    phone = usePhone(),
-    shippingPhone =
-      ShippingAddress?.phone === undefined ? "" : ShippingAddress?.phone;
+  ].map((z) => z.split(","));
 
-  async function changePreValues(key, value) {
-    const attribute = await changeAttribute({
-      type: "updateAttribute",
-      key: `${key}`,
-      value: `${value}`,
-    });
-    // console.log('attribute: ', attribute);
+  /* ---------------- HELPERS ---------------- */
+  function formatPhone(phone) {
+    if (!phone) return "";
+    return phone.replace(/^\+91/, "").replace(/^0/, "");
   }
-  async function updateAddress(address) {
-    const code = await changeAddress({
-      type: "updateShippingAddress",
-      address: address,
-    });
-    // console.log("code", code);
-    if(code.type === "success") {
-      changePreValues("first_name", "");
-      changePreValues("phone", "");
-    }
-  }
+
+  const prepaidAttr = Attributes.find((a) => a.key === "prepaid");
+  const currentPrepaid = prepaidAttr?.value;
   const zipcode = ShippingAddress?.zip;
-  if (zipArrays.some((zipArray) => zipArray.includes(zipcode)) === false) {
-    async function change() {
-      const attribute = await changeAttribute({
+  const shippingPhone = formatPhone(ShippingAddress?.phone);
+
+  /* ---------------- PREPAID LOGIC (SAFE) ---------------- */
+  useEffect(() => {
+    if (!zipcode) return;
+
+    const zipAllowed = zipArrays.some((z) => z.includes(zipcode));
+    const phoneRestricted = restrictPhones.includes(shippingPhone);
+
+    const shouldBePrepaid =
+      zipAllowed && !phoneRestricted ? "true" : "false";
+
+    if (currentPrepaid !== shouldBePrepaid) {
+      changeAttribute({
         type: "updateAttribute",
         key: "prepaid",
-        value: "false",
+        value: shouldBePrepaid,
       });
-      // console.log('attribute: ', attribute);
     }
-    if (lastAttribute.length === 0 || lastAttribute[0]?.value === "true") {
-      change();
-    }
-  } else {
-    if (restrict_phones.includes(formatPhoneNumber(shippingPhone)) === true) {
-      async function change() {
-        const attribute = await changeAttribute({
-          type: "updateAttribute",
-          key: "prepaid",
-          value: "false",
-        });
-        // console.log('attribute: ', attribute);
-      }
-      if (lastAttribute.length === 0 || lastAttribute[0]?.value === "true") {
-        change();
-      }
-    } else {
-      async function change() {
-        const attribute = await changeAttribute({
-          type: "updateAttribute",
-          key: "prepaid",
-          value: "true",
-        });
-        // console.log('attribute: ', attribute);
-      }
-      if (lastAttribute.length === 0 || lastAttribute[0]?.value === "false") {
-        change();
-        // console.log('l', lastAttribute[0]);
-      }
-    }
-  }
+  }, [zipcode, shippingPhone]);
+
+  /* ---------------- AUTO ADDRESS FILL (SAFE) ---------------- */
   useEffect(() => {
-    var address = {};
-    if (Attributes.some((attribute) => attribute.key === "first_name")) {
-      const name = Attributes.find(
-        (attribute) => attribute.key === "first_name"
-      );
-      if (name.value !== "") {
-        const firstName = name.value.split(" ")[0];
-        const lastName = name.value.split(" ")[1];
-        if (lastName !== "undefined" || firstName === lastName) {
-          address.firstName = firstName;
-        } else {
-          address.firstName = firstName;
-          address.lastName = lastName;
-        }
-      }
+    if (addressUpdatedRef.current) return;
+
+    const firstNameAttr = Attributes.find((a) => a.key === "first_name");
+    const phoneAttr = Attributes.find((a) => a.key === "phone");
+
+    let address = {};
+
+    if (firstNameAttr?.value) {
+      const [firstName, lastName] = firstNameAttr.value.split(" ");
+      address.firstName = firstName;
+      if (lastName) address.lastName = lastName;
     }
-    if (Attributes.some((attribute) => attribute.key === "phone")) {
-      const phone = Attributes.find((attribute) => attribute.key === "phone");
-      if (phone.value !== "") {
-        address.phone = formatPhoneNumber(phone.value);
-        if (formatPhoneNumber(phone.value).length === 10) {
-          address.countryCode = "IN";
-        } else if (formatPhoneNumber(phone.value).length === 9) {
-          address.countryCode = "AE";
-        }
-      }
+
+    if (phoneAttr?.value) {
+      const formatted = formatPhone(phoneAttr.value);
+      address.phone = formatted;
+      address.countryCode = formatted.length === 10 ? "IN" : "AE";
     }
-    if (Object.keys(address).length !== 0) {
-      updateAddress(address);
+
+    if (Object.keys(address).length > 0) {
+      addressUpdatedRef.current = true;
+      changeAddress({
+        type: "updateShippingAddress",
+        address,
+      });
     }
   }, [Attributes]);
+
+  /* ---------------- VALIDATION ---------------- */
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
-    const regex = /^(?:\+91)?[6789][0-9]{4}([ ]?)[0-9]{5}$/;
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,12}$/i;
-    const nameRegex = /^[A-Za-z]*$/;
-    return email === undefined && phone === undefined
-      ? {
-          behavior: "block",
-          reason: "Email or phone is mandatory",
-          errors: [
-            {
-              message: "Please enter email or phone number",
-            },
-          ],
-        }
-      : email !== undefined &&
-        emailRegex.test(email) === false &&
-        phone === undefined
-      ? {
-          behavior: "block",
-          reason: "Invalid Email",
-          errors: [
-            {
-              message: "Please enter valid email",
-              target: "$.cart.buyerIdentity.email",
-            },
-          ],
-        }
-      : email === undefined &&
-        phone !== undefined &&
-        regex.test(phone) === false
-      ? {
-          behavior: "block",
-          reason: "Invalid Phone",
-          errors: [
-            {
-              message: "Please enter valid phone",
-              target: "$.cart.buyerIdentity.phone",
-            },
-          ],
-        }
-      : ShippingAddress.firstName === undefined
-      ? {
-          behavior: "block",
-          reason: "Invalid First Name",
-          errors: [
-            {
-              message: "Please enter first name",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.firstName",
-            },
-          ],
-        }
-      : ShippingAddress?.firstName !== undefined &&
-        nameRegex.test(ShippingAddress?.firstName) === false
-      ? {
-          behavior: "block",
-          reason: "Invalid First Name",
-          errors: [
-            {
-              message: "Please enter valid first name",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.firstName",
-            },
-          ],
-        }
-      : ShippingAddress.lastName === undefined
-      ? {
-          behavior: "block",
-          reason: "Invalid Last Name",
-          errors: [
-            {
-              message: "Please enter last name",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.lastName",
-            },
-          ],
-        }
-      : ShippingAddress.lastName !== undefined &&
-        nameRegex.test(ShippingAddress?.lastName) === false
-      ? {
-          behavior: "block",
-          reason: "Invalid Last Name",
-          errors: [
-            {
-              message: "Please enter last name",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.lastName",
-            },
-          ],
-        }
-      : ShippingAddress?.address1 === undefined
-      ? {
-          behavior: "block",
-          reason: "Address1 empty",
-          errors: [
-            {
-              message: "Please enter address",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.address1",
-            },
-          ],
-        }
-      : ShippingAddress.address1 !== undefined &&
-        ShippingAddress.address1.length < 15
-      ? {
-          behavior: "block",
-          reason: "Address1 Invalid",
-          errors: [
-            {
-              message: "Please enter atleast 15 characters",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.address1",
-            },
-          ],
-        }
-      : ShippingAddress?.phone === undefined
-      ? {
-          behavior: "block",
-          reason: "Phone number empty",
-          errors: [
-            {
-              message: "Please enter a phone number",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.phone",
-            },
-          ],
-        }
-      : ShippingAddress?.phone !== undefined &&
-        regex.test(ShippingAddress?.phone) === false
-      ? {
-          behavior: "block",
-          reason: "Invalid Phone number",
-          errors: [
-            {
-              message: "Please enter valid number",
-              target: "$.cart.deliveryGroups[0].deliveryAddress.phone",
-            },
-          ],
-        }
-      : {
-          behavior: "allow",
-        };
+    if (!canBlockProgress) return { behavior: "allow" };
+
+    const phoneRegex = /^(?:\+91)?[6789][0-9]{9}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const nameRegex = /^[A-Za-z]+$/;
+
+    if (!email && !phone) {
+      return {
+        behavior: "block",
+        errors: [{ message: "Email or phone is required" }],
+      };
+    }
+
+    if (email && !emailRegex.test(email)) {
+      return {
+        behavior: "block",
+        errors: [{ message: "Invalid email" }],
+      };
+    }
+
+    if (phone && !phoneRegex.test(phone)) {
+      return {
+        behavior: "block",
+        errors: [{ message: "Invalid phone number" }],
+      };
+    }
+
+    if (
+      !ShippingAddress?.firstName ||
+      !nameRegex.test(ShippingAddress.firstName)
+    ) {
+      return {
+        behavior: "block",
+        errors: [{ message: "Invalid first name" }],
+      };
+    }
+
+    if (
+      !ShippingAddress?.lastName ||
+      !nameRegex.test(ShippingAddress.lastName)
+    ) {
+      return {
+        behavior: "block",
+        errors: [{ message: "Invalid last name" }],
+      };
+    }
+
+    return { behavior: "allow" };
   });
+
   return null;
 }
--- extensions/traya-checkout-dev/src/ThankYou.jsx
+++ extensions/traya-checkout-dev/src/ThankYou.jsx
@@ -9,82 +9,71 @@ import {
   View,
 } from "@shopify/ui-extensions-react/checkout";
 
-const thankYouBlock = reactExtension("purchase.thank-you.block.render", () => (
-  <Attribution />
-));
-export { thankYouBlock };
+export const thankYouBlock = reactExtension(
+  "purchase.thank-you.block.render",
+  () => <Attribution />
+);
 
 function Attribution() {
-  var { banner_image, banner_image2 } = useSettings();
-  if (
-    banner_image === "" ||
-    banner_image === undefined ||
-    banner_image === null
-  ) {
-    banner_image =
-      "https://cdn.shopify.com/s/files/1/0699/2199/7058/files/Group_1000006344.png?v=1718089642";
-  }
-  if (
-    banner_image2 === "" ||
-    banner_image2 === undefined ||
-    banner_image2 === null
-  ) {
-    banner_image2 =
-      "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/Group_102353309_1.png";
-  }
-  
-  const Attributes = useAttributes();
-  const lastAttribute = Attributes.filter((attribute) => {
-    if (attribute.key === "caseid") {
-      return attribute;
-    }
-  });
+  const { banner_image } = useSettings();
+  const attributes = useAttributes();
+
+  const gender =
+    attributes
+      .find((attr) => attr.key === "user__gender")
+      ?.value?.toLowerCase() || null;
+
+  const caseId = attributes.find((attr) => attr.key === "caseid")?.value;
+
+  const rescheduleUrl = caseId
+    ? `https://form.traya.health/pages/reschedule-slot/${caseId}?orderPlatform=shopify`
+    : `https://form.traya.health/pages/reschedule-slot?orderPlatform=shopify`;
+
+  const bookCallBanner =
+    banner_image ||
+    "https://cdn.shopify.com/s/files/1/0699/2199/7058/files/Group_1000006344.png?v=1718089642";
+
+  const downloadBanner =
+    gender === "female"
+      ? "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/final_app_download.gif?v=1766412635"
+      : "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/Group_102353309_1.png";
 
   return (
     <>
-      <View
-        inlineSize="fill"
-        background="subdued"
-        border="base"
-        borderRadius="base"
-      >
-        <InlineLayout columns={"fill"}>
-          <Pressable
-            inlineAlignment={`center`}
-            to={`${
-              lastAttribute.length === 0
-                ? `https://form.traya.health/pages/reschedule-slot?orderPlatform=shopify`
-                : `https://form.traya.health/pages/reschedule-slot/${lastAttribute[0].value}?orderPlatform=shopify`
-            }`}
-          >
+      {/* BOOK A CALL — ALWAYS */}
+      <View inlineSize="fill" background="subdued" border="base" borderRadius="base">
+        <InlineLayout columns="fill">
+          <Pressable inlineAlignment="center" to={rescheduleUrl}>
             <Image
-              source={banner_image}
+              source={bookCallBanner}
               loading="eager"
               fit="cover"
-              accessibilityRole="Book a call"
+              accessibilityRole="image"
               accessibilityDescription="Book a call"
             />
           </Pressable>
         </InlineLayout>
       </View>
+
       <BlockSpacer />
+
+      {/* DOWNLOAD APP — ALWAYS (image changes by gender) */}
       <View
         inlineSize="fill"
         background="subdued"
         border="base"
         borderRadius="base"
       >
-        <BlockSpacer />
-        <InlineLayout columns={"fill"}>
+        <InlineLayout columns="fill">
           <Pressable
-            inlineAlignment={`center`}
+            inlineAlignment="center"
             to="https://trayahealth.app.link/xT3UrtZDvyb"
           >
             <Image
-              source={banner_image2}
+              source={downloadBanner}
               loading="eager"
               fit="cover"
-              accessibilityRole="Download App"
+              accessibilityRole="image"
               accessibilityDescription="Download App"
             />
           </Pressable>
